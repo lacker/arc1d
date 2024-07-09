21d11179b5038296482521518bce3b5e98cba906
Kevin Lacker <lacker@gmail.com>
2024-07-09 13:15:57 -0700

    grouping

--- data.py
+++ data.py
@@ -2,6 +2,25 @@ import os
 
 data_path = os.path.join(os.path.dirname(__file__), "data.txt")
 
+def group_lines(is_separator, lines):
+  groups = []
+  pending_group = []
+  for line in lines:
+    if is_separator(line) and pending_group:
+      groups.append(pending_group)
+      pending_group = []
+  if pending_group:
+    groups.append(pending_group)
+  return groups
+
+
 def read():
+  lines = []
   for line in open(data_path):
-    pass
+    if line.startswith("#"):
+      continue
+    lines.append(line)
+
+  # TODO
+
+      
